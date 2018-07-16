9fccd9fa57c146a39593dfe2e29429e38131db56
mica <mica.eldridge1@gmail.com>
2018-07-16 15:55:35 -0700

    added key-based access

--- ensure_ultrasonic_program_running.bash
+++ ensure_ultrasonic_program_running.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-ps aux | grep [u]ltrasonic_distance_reader.py || {
+ps aux | grep -v grep | grep [u]ltrasonic_distance_reader.py >/dev/null || {
     /usr/bin/python3 /home/pi/git/car_detector/ultrasonic_distance_reader.py
 }
